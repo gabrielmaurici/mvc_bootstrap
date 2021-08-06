2e9475385ba5806175261030b54a54af2f7ff10a
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-06 14:49:55 -0300

    Crud MVC Bootstrap

--- WebMvcBoot/WebMvcBoot/Controllers/ViagensController.cs
+++ WebMvcBoot/WebMvcBoot/Controllers/ViagensController.cs
@@ -3,18 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMvcBoot.Models;
+using WebMvcBoot.Repository;
 
 namespace WebMvcBoot.Controllers
 {
     public class ViagensController : Controller
     {
+        RepositoryViagens repository = new RepositoryViagens();
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Cadastar()
+        [HttpGet]
+        public ActionResult Cadastrar()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Cadastrar(Viagens model)
+        {
+            repository.Cadastrar(model);
+            ModelState.Clear();
+            return View();
+        }
+        public ActionResult Listar()
+        {
+           return View(repository.Listar());
+        }
+        [HttpGet]
+        public ActionResult Detalhes(int id)
+        {
+            return View(repository.Listar(id));
+        }
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            return View(repository.Listar(id));
+        }
+        [HttpPost]
+        public ActionResult Editar(Viagens model)
+        {
+            repository.Editar(model);
+            return RedirectToAction("Listar");
+        }
+        [HttpGet]
+        public ActionResult Deletar(int id)
+        {
+            repository.Deletar(id);
+            return RedirectToAction("Listar");
+        }
+
     }
 }
---
+++ WebMvcBoot/WebMvcBoot/Repository/RepositoryViagens.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMvcBoot.Models;
+
+namespace WebMvcBoot.Repository
+{
+    public class RepositoryViagens
+    {
+        static List<Viagens> listaViagens = new List<Viagens>();
+        static int id = 1;
+        public void Cadastrar(Viagens model)
+        {
+            model.Id = id;
+            id++;
+            listaViagens.Add(model);
+        }
+        public List<Viagens> Listar()
+        {
+            return listaViagens;
+        }
+        public Viagens Listar(int id)
+        {
+            return listaViagens.Find(v => v.Id == id) ;
+        }
+        public void Editar(Viagens model)
+        {
+            int index = listaViagens.FindIndex(v => v.Id == model.Id);
+            listaViagens[index] = model;
+        }
+        public void Deletar(int id)
+        {
+            listaViagens.Remove(Listar(id));
+        }
+    }
+}
